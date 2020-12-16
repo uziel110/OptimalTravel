dba559d51fce66f1d7da274dfaa071549a8970d8
binim10 <57068936+binim10@users.noreply.github.com>
2020-12-16 14:37:43 +0200

    mapView complete without directions

--- app/src/main/java/com/example/optimaltravel/UI/MapsActivity.java
+++ app/src/main/java/com/example/optimaltravel/UI/MapsActivity.java
@@ -3,27 +3,37 @@ package com.example.optimaltravel.UI;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.optimaltravel.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.libraries.places.api.model.Place;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Button btGetDirections;
+    List<MarkerOptions> points = new LinkedList<MarkerOptions>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        btGetDirections = findViewById(R.id.btGD);
+        MapFragment mapFragment1 = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment1.getMapAsync(this);
+
     }
 
     /**
@@ -37,11 +47,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap = googleMap;// Add a marker in Sydney and move the camera
+        mMap.clear();
+        for (String it : CreatePath.keysList) {
+            points.add(new MarkerOptions().position(CreatePath.keyMap.get(it)));
+        }
+        for (int i = 0; i < points.size(); i++) {
+            mMap.addMarker(points.get(i));
+        }
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(points.get(1).getPosition()));
+        mMap.getMaxZoomLevel();
     }
 }
